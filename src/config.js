0982814df6d2a975f6f906a88693d3ded2a4bf8d
VT <vivekanand.thite@synerzip.com>
2018-06-15 19:31:19 +0530

    Added settings page

--- src/config.js
+++ src/config.js
@@ -1,34 +1,34 @@
 const dev = {
   s3: {
       REGION: "us-east-1",
-      BUCKET: "tvs-notes"
+      BUCKET: "notes-app-api-dev-attachmentbucket-1klg2u6on1rd3"
     },
   apiGateway: {
       REGION: "us-east-1",
-      URL: "https://ivnjm68lzi.execute-api.us-east-1.amazonaws.com/dev"
+      URL: "https://pivn1edth6.execute-api.us-east-1.amazonaws.com/dev"
     },
   cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_W5d394nhe",
-      APP_CLIENT_ID: "4eq460m43l5604kvjaua9ecqsl",
-      IDENTITY_POOL_ID: "us-east-1:334e0ce7-e94c-4223-bf06-3045425b2434"
+      USER_POOL_ID: "us-east-1_U0pfNAKu8",
+      APP_CLIENT_ID: "15dj46m7fus8vtc6v318seu2ip",
+      IDENTITY_POOL_ID: "us-east-1:c4080ce3-8ed9-4017-899b-0e9bcd65da3b"
     }
 };
 
 const prod = {
   s3: {
       REGION: "us-east-1",
-      BUCKET: "tvs-notes"
+      BUCKET: "notes-app-api-prod-attachmentbucket-d44d6rtqu9tb"
     },
   apiGateway: {
       REGION: "us-east-1",
-      URL: "https://ivnjm68lzi.execute-api.us-east-1.amazonaws.com/dev"
+      URL: "https://iht5rt5bnj.execute-api.us-east-1.amazonaws.com/prod"
     },
   cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_W5d394nhe",
-      APP_CLIENT_ID: "4eq460m43l5604kvjaua9ecqsl",
-      IDENTITY_POOL_ID: "us-east-1:334e0ce7-e94c-4223-bf06-3045425b2434"
+      USER_POOL_ID: "us-east-1_khYu4LlgX",
+      APP_CLIENT_ID: "2hnu4nt45k94onvh4psc3td78a",
+      IDENTITY_POOL_ID: "us-east-1:a22353df-fd40-4b33-a249-626aaa1df1ae"
     }
 };
 
