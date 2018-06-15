bcf38140e90ed8a6c64a89ae6043319e37c83810
VT <vivekanand.thite@synerzip.com>
2018-06-15 17:49:28 +0530

    configured staging enviornments - dev and prod

--- src/config.js
+++ src/config.js
@@ -1,19 +1,43 @@
-export default {
-    
-    MAX_ATTACHMENT_SIZE: 5000000,
-
-    s3: {
+const dev = {
+  s3: {
       REGION: "us-east-1",
       BUCKET: "tvs-notes"
     },
-    apiGateway: {
+  apiGateway: {
       REGION: "us-east-1",
       URL: "https://ivnjm68lzi.execute-api.us-east-1.amazonaws.com/dev"
     },
-    cognito: {
+  cognito: {
       REGION: "us-east-1",
       USER_POOL_ID: "us-east-1_W5d394nhe",
       APP_CLIENT_ID: "4eq460m43l5604kvjaua9ecqsl",
       IDENTITY_POOL_ID: "us-east-1:334e0ce7-e94c-4223-bf06-3045425b2434"
     }
-  };
+};
+
+const prod = {
+  s3: {
+      REGION: "us-east-1",
+      BUCKET: "tvs-notes"
+    },
+  apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ivnjm68lzi.execute-api.us-east-1.amazonaws.com/dev"
+    },
+  cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_W5d394nhe",
+      APP_CLIENT_ID: "4eq460m43l5604kvjaua9ecqsl",
+      IDENTITY_POOL_ID: "us-east-1:334e0ce7-e94c-4223-bf06-3045425b2434"
+    }
+};
+
+//default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+
+export default {    
+    // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config    
+};
